0a18004c3ddf9fbeb28a80c07cf8a59ec18264cd
da0 <kimda0@inha.edu>
2024-01-25 21:53:31 +0900

    add MLC train

--- README.md
+++ README.md
@@ -1,38 +1,46 @@
-
-## Testing for YOLOv7
-
-``` shell
-python yolo_test.py --data data/coco.yaml --img 640 --batch 32 --conf 0.001 --iou 0.65 --device 0 --weights yolov7.pt --name yolov7_640_val
-```
-
-## Testing for MLC
-
-``` shell
-python test_for_MLC.py --data data/coco.yaml --img 640 --batch 32 --conf 0.001 --iou 0.65 --device 0 --weights yolov7.pt --name yolov7_640_val
-```
-
-## Training
-
-Single GPU training
+## 1. Training (labeled data)
 
 ``` shell
 # train p5 models
-python train.py --workers 8 --device 0 --batch-size 32 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+python train.py --workers 8 --device 0 --batch-size 4 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
 
 ```
 
-## Crop the traffic light images
+## 2. Crop the traffic light images
 
 On image:
 ``` shell
 python tl_crop.py --weights weights/onlytl.pt --conf 0.25 --img-size 640 --source inference/testset
 ```
-## Get results MLC
+
+## 3. Training for Multi-Label Classification(MLC)
+
+``` shell
+python train_mlc.py --workers 8 --device 0 --batch-size 4 --data data/coco.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+
+```
+
+## 4. Get results MLC
 
 TODO : MLC 결과의 컨피던스를 설정하는 부분 추가 
 
 ``` shell
 python detect_with_MLC.py --weights weights/onlytl.pt --conf 0.25 --save-txt --save-conf --img-size 640 --source inference/testset
+```
+
+## 5. Testing for MLC
+
+``` shell
+python test_for_MLC.py --device cpu 
+```
+TODO : make 1 confidence for gt
+
+==============================================================
+
+## Testing for YOLOv7
+
+``` shell
+python yolo_test.py --data data/coco.yaml --img 640 --batch 32 --conf 0.001 --iou 0.65 --device 0 --weights yolov7.pt --name yolov7_640_val
 ```
 
 ## Export
---
+++ train_mlc.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import os
+import shutil
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense, Dropout, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+
+def load_data(label_path, image_dir):
+    # Load data from CSV file
+    data = pd.read_csv(label_path)
+    print(data.shape)
+    print(data.head())
+
+    img_width = 150
+    img_height = 150
+
+    X = []
+
+    # Load and preprocess images
+    for i in tqdm(range(data.shape[0])):
+        path = os.path.join('only_songdo_cropp_datset/integrate_only_songdo', data['Id'][i] + '.jpg')
+        img = image.load_img(path, target_size=(img_width, img_height, 3))
+        img = image.img_to_array(img)
+        img = img / 255.0
+        X.append(img)
+
+    X = np.array(X)
+    # print(X.shape)
+    y = data.drop(['Id', 'State'], axis=1)
+    y = y.to_numpy()
+    # print(y.shape)
+
+    return X, y
+
+def gpu_check():
+    # GPU 장치 목록 확인
+    physical_devices = tf.config.list_physical_devices('GPU')
+    if len(physical_devices) > 0:
+        print("사용 가능한 GPU 장치 목록:")
+        for device in physical_devices:
+            print(device)
+    else:
+        print("사용 가능한 GPU 장치가 없습니다.")
+
+
+def build_model(input_shape, num_classes):
+
+    # Build the model
+    model = Sequential([
+        Conv2D(16, (3,3), activation='relu', input_shape=input_shape),
+        MaxPool2D(2, 2),
+        Conv2D(32, (3,3), activation='relu'),
+        MaxPool2D(2, 2),
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPool2D(2, 2),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dropout(0.5),
+        Dense(y_train.shape[1], activation='sigmoid')
+    ])
+
+    print(model.summary())
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def train_model(model, X_train, y_train, epochs=16, batch_size=16, X_test=None, y_test=None):
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test))
+    return history
+
+def save_model(model, save_directory, model_weights_filename):
+    os.makedirs(save_directory, exist_ok=True)
+    model.save(os.path.join(save_directory, model_weights_filename))
+    print(f"Model weights saved as '{model_weights_filename}'")
+
+label_path = 'only_songdo_cropp_datset/only_songdo.csv'
+image_dir = 'only_songdo_cropp_datset/integrate_only_songdo'
+model_weights_filename = 'songdo_images_weight.h5'
+
+
+X, y = load_data(label_path, image_dir)
+gpu_check()
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.15)
+# print(X_train)
+model = build_model(X_train[0].shape, y_train.shape[1])
+history = train_model(model, X_train, y_train, epochs=16, batch_size=32, X_test=X_test, y_test=y_test)
+save_model(model, 'weights', model_weights_filename)
+
+
+# # # Plot training & validation accuracy values
+# # epoch_range = range(1, epochs+1)
+# # plt.plot(epoch_range, history.history['accuracy'])
+# # plt.plot(epoch_range, history.history['val_accuracy'])
+# # plt.title('Model accuracy')
+# # plt.ylabel('Accuracy')
+# # plt.xlabel('Epoch')
+# # plt.legend(['Train', 'Val'], loc='upper left')
+# # plt.show()
+
+# # # Plot training & validation loss values
+# # plt.plot(epoch_range, history.history['loss'])
+# # plt.plot(epoch_range, history.history['val_loss'])
+# # plt.title('Model loss')
+# # plt.ylabel('Loss')
+# # plt.xlabel('Epoch')
+# # plt.legend(['Train', 'Val'], loc='upper left')
+# # plt.show()
+
+# # # Plot learning curve
+# # # plot_learningCurve(history, epochs)
+
